d4ac8f8547a90d14a8f17fbbb4429ee48a7207e0
Colin <colin.diesh@gmail.com>
2018-12-29 19:29:49 -0500

    Updates

---
+++ precache-manifest.a81ecb2b94d2c961b91d33fdf4de969e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8908646da42cd9cf3230",
+    "url": "/static/js/main.8908646d.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "8908646da42cd9cf3230",
+    "url": "/static/css/main.4704536d.chunk.css"
+  },
+  {
+    "revision": "f4f1890d484ff971c633407ac6f1c996",
+    "url": "/index.html"
+  }
+];

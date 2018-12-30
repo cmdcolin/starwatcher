883e65c0068deaafe6e9bfa587aac2c6b2c1bafb
Colin <colin.diesh@gmail.com>
2018-12-29 19:03:38 -0500

    Add app structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,108 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-class App extends Component {
+class NameForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: '' }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value })
+  }
+
+  handleSubmit(event) {
+    console.log('here',this.state.value)
+    this.props.handleSubmit(this.state.value)
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Enter your GitHub username{' '}
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    )
   }
 }
 
-export default App;
+class StarList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hits: [],
+      isLoading: false,
+      error: null,
+    }
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true })
+    console.log('props name', this.props.name)
+    fetch(`https://api.github.com/users/${this.props.name}/starred`)
+          .then(res => res.json())
+          .then((starred) => starred.map((s) => ({
+              owner:       s.owner.login
+            , repo:        s.name
+            , description: s.description
+            , language:    s.language
+            , isFork:      false
+            , stargazers:  s.stargazers_count
+            , watchers:    s.watchers_count
+          })))
+          .then(data => this.setState({ hits: data, isLoading: false }))
+          .catch(error => this.setState({ error, isLoading: false }));
+  }
+  render() {
+    const { hits, isLoading } = this.state
+
+    if (isLoading) {
+      return <p>Loading ...</p>
+    }
+    console.log(hits)
+    return (
+      <ul>
+        {hits.map(hit => (
+          <li key={hit.objectID}>
+            <a href={hit.url}>{hit.title}</a>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {name: null}
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(name) {
+    this.setState(state => ({ name }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">starwatcher</header>
+        <NameForm handleSubmit={this.handleSubmit} />
+        <StarList name={this.state.name} />
+      </div>
+    )
+  }
+}
+
+export default App
